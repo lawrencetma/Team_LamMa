71a259753566a70540153f0286eae6d138737371
Lawrence Ma <lawrencema@lawrences-air.att.net>
2015-03-26 23:02:35 -0700

    last js fix

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,16 +1,17 @@
-var ready;
+var ready,
+position;
 ready = function(){
-
   (function(){
 
     var parallax = document.querySelectorAll(".ground"),
-        speed = -0.1;
+        speed = -0.1,
+        position = parseInt($('.intro-section').css('padding'));
 
     window.onscroll = function(){
       [].slice.call(parallax).forEach(function(el,i){
 
         var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "0% " + (windowYOffset * speed) + "px";
+            elBackgrounPos = position + "% " + (windowYOffset * speed) + "px";
 
         el.style.backgroundPosition = elBackgrounPos;
 
